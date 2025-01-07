f507c60c3154d633f00c834af5dc7cd0e0a0df25
jerome-cossu <jerome.cossu@laplateforme.io>
2025-01-07 15:10:22 +0100

    modification conde yoel, ajout alarme

--- esaaie.py
+++ esaaie.py
@@ -1,18 +1,20 @@
 from datetime import datetime
 import time
 def alarme():
-    heure_reveil = input("Regler un reveil (HH:MM:SS) :")
-    try :
-        time.strptime (heure_reveil, "%H:%M:%S")
+    heure_reveil = input("Régler un réveil (HH:MM:SS) :")
+    try:
+        time.strptime(heure_reveil, "%H:%M:%S")
         return heure_reveil
     except ValueError:
-        print("Heure non valide, entrer une heure valide .")
+        print("Heure non valide, entrer une heure valide.")
         return alarme()
-alarme()
-def afficher_heure():
+def afficher_heure(heure_reveil):
     while True:
         now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")    
-        print(current_time, end="\r")  
-        time.sleep(1)
-afficher_heure()
+        heure_actuel = now.strftime("%H:%M:%S")
+        print(heure_actuel, end="\r")  
+        time.sleep(1)  
+        if heure_actuel == heure_reveil:
+            print("\nRéveil ! L'heure est arrivée.")
+heure_reveil = alarme()
+afficher_heure(heure_reveil) 
