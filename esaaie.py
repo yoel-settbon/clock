4a61bfc65dc9261aee3e4c841675be3f2348cc11
jerome-cossu <jerome.cossu@laplateforme.io>
2025-01-07 16:34:55 +0100

    ajout d'un menu de navigation

--- esaaie.py
+++ esaaie.py
@@ -16,48 +16,5 @@ def afficher_heure(heure_reveil):
         time.sleep(1)  
         if heure_actuel == heure_reveil:
             print("\nRéveil ! L'heure est arrivée.")
-def horloge():
-    heure_debut = input("Entrez l'heure de départ HH:MM:SS :")    
-    try:
-        h, m, s = map(int, heure_debut.split(':'))
-        if not (0 <= h < 24 and 0 <= m < 60 and 0 <= s < 60):
-            print("Heure invalide. Veuillez entrer une heure valide .")
-            return
-    except ValueError:
-        print("Format invalide. Veuillez entrer l'heure au format HH:MM:SS .")
-        return 
-    while True:
-        print(f"{h:02}:{m:02}:{s:02}", end='\r')
-        time.sleep(1)
-        s += 1
-        if s == 60:
-            s = 0
-            m += 1
-        if m == 60:
-            m = 0
-            h += 1
-        if h == 24:
-            h = 0
-def menu():
-    print ("____MENU____")
-    print ("1 : Afficher l'heure actuelle ")
-    print ("2 : Régler une heure ")
-    print ("3 : Régler une alarme ")
-    print ("4 : Quitter ")
-    choix = input("Faites votre choix (1, 2, 3, 4) : ")
-    if choix == "1" :
-        print("\nIl est actuellement :")
-        afficher_heure(None)
-    elif choix == "2" :
-        print("\nEntrée l'heure de votre choix :")
-        horloge()
-    elif choix == "3" :
-        heure_reveil = alarme()
-        afficher_heure(heure_reveil)
-    elif choix == "4" :
-        print("Au revoir !")
-        exit()
-    else:
-        print("Choix invalide. Essayez à nouveau.")
-menu()
-
+heure_reveil = alarme()
+afficher_heure(heure_reveil)
