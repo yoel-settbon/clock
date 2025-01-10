7ea2855df97e0a63d64d3f67e1faf929ccf0013e
jerome-cossu <jerome.cossu@laplateforme.io>
2025-01-10 10:56:30 +0100

    :memo: code fonctionnel, ajout commentaire manque à ajouter sonnerie alarme, et touche pour stopper la sonnerie et arrêter le temps

--- essaie.py
+++ essaie.py
@@ -1,56 +1,85 @@
-from datetime import datetime
-import time
-import os
-from time import sleep
+from datetime import datetime   # importation pour la manipulation du temps
+import time                     # importation pour gérer les pauses
+import os                       #importation des commandes système
+from time import sleep          # importation des pauses pour faire des pause dans le programme
+
 def alarme():
+
+            # demande à l'utilisateur de régler une alarme
     heure_reveil = input("Régler un réveil (HH:MM:SS AM/PM) :")
     try:
+            # s'assure que l'heure entrée est au format voulu AM/PM
         time.strptime(heure_reveil, "%I:%M:%S %p")
         return heure_reveil
     except ValueError:
+            # si le format est invalide un message d'erreur s'affiche et redemande de rentrer l'heure
         print("Heure non valide, entrer une heure valide.")
         return alarme()
+
 def afficher_heure(heure_reveil):
+
+            # boucle infinie pour afficher l'heure actuelle
     while True:
+            # l'heure du système est récupérée et utilisé
         now = datetime.now()
+            # converstion de l'heure en format AM/PM
         heure_actuel = now.strftime("%I:%M:%S %p") 
         print(heure_actuel, end="\r")  
-        time.sleep(1)  
+        time.sleep(1)
+            # si l'heure actuelle correspond à l'heure réglé pour l'alarme, le message voulu s'affiche  
         if heure_actuel == heure_reveil:
             print("\nDebout, FEIGNAAAAASSEEEE")
+
 def horloge():
+
+            # l'utilisateur peux rentrer l'heure de départ souhatée
     heure_debut = input("Entrez l'heure de départ HH:MM:SS :")    
     try:
+            # permet de séparer l'heure entrée par l'utilisateur au format HH:MM:SS
         h, m, s = map(int, heure_debut.split(':'))
         if not (0 <= h < 24 and 0 <= m < 60 and 0 <= s < 60):
-            print("Heure invalide. Veuillez entrer une heure valide .")
-            return
+            # si l'heure est invalide, on redemande à l'utilisateur de rentrer une heure valide
+            print("Heure invalide. Veuillez entrer une heure valide :")
+            return horloge() 
     except ValueError:
-        print("Format invalide. Veuillez entrer l'heure au format HH:MM:SS .")
-        return 
+            # si l'entrée n'est pas au bon format, on affiche un message d'erreur et on redemande à l'utilisateur de rentrer une heure valide
+        print("Format invalide. Veuillez entrer l'heure au format HH:MM:SS :")
+        return horloge()     
     while True:
+            # creer une date avec l'heure actuelle, remplaçant les heures, les minutes et les secondes par celles de l'utilisateur
         heure_formatee = datetime.now().replace(hour=h, minute=m, second=s).strftime("%I:%M:%S %p") 
+            # affiche l'heure formatée
         print(heure_formatee, end='\r')
+            # actualise l'heure toutes les secondes
         time.sleep(1)
+            # incrémente les second
         s += 1
+            # dès que les seconde, minutes atteignent 60, on les remet à 0 et on incrémente les minutes et les heures
         if s == 60:
             s = 0
             m += 1
         if m == 60:
             m = 0
             h += 1
+            # quand les heures atteignent 24, on les remet à 0
         if h == 24:
             h = 0
+
 def menu():
+    
+            # permet d'afficher le menu de l'horloge
     print ("______MENU_DE_L'HORLOGE______")
     print ("1 : Afficher l'heure actuelle ")
     print ("2 : Régler une heure ")
     print ("3 : Régler une alarme ")
     print ("4 : Quitter ")
     print ("_____________________________")
+            # permet à l'utilisateur de faire un choix dans le menu
     choix = input("Faites votre choix (1, 2, 3, 4) : ")
+            # une fois le choix fait, on efface l'écran
     sleep(1)
     os.system('cls')
+            # une fois le choix fait, ça nous redirige vers la fonction correspondante
     if choix == "1" :
         print("\nIl est actuellement :")
         afficher_heure(None)
@@ -61,9 +90,11 @@ def menu():
         heure_reveil = alarme()
         afficher_heure(heure_reveil)
     elif choix == "4" :
+            # si l'utilisateur choisit de quitter, un message d'au revoir s'affiche et le programme se ferme
         print("Au revoir !")
         exit()
     else:
         print("Choix invalide. Essayez à nouveau.")
 
+            # rappel la fonction menu de l'horloge
 menu()
--- test.py
+++ test.py
@@ -1,68 +1,113 @@
-from datetime import datetime
-import time
-import os
-from time import sleep
+from datetime import datetime  
+import time  
+import os  
+from time import sleep 
+
 def alarme():
-    heure_reveil = input("Régler un réveil (HH:MM:SS) :")
+    # Demande à l'utilisateur de régler une alarme avec le format HH:MM:SS AM/PM
+    heure_reveil = input("Régler un réveil (HH:MM:SS AM/PM) :")
+    
     try:
-        time.strftime(heure_reveil, "%H:%M:%S")
-        return heure_reveil
+        # Vérifie si l'heure entrée est bien dans le format 12h AM/PM
+        time.strptime(heure_reveil, "%I:%M:%S %p")
+        return heure_reveil  # Si c'est valide, retourne l'heure entrée
     except ValueError:
+        # Si l'heure est invalide, on affiche un message d'erreur et on redemande l'entrée
         print("Heure non valide, entrer une heure valide.")
-        return alarme()
+        return alarme()  # Relance la fonction pour demander à nouveau l'heure
+
 def afficher_heure(heure_reveil):
     while True:
+        # Récupère l'heure actuelle du système
         now = datetime.now()
-        heure_actuel = now.strftime("%H:%M:%S")
+        # Formate l'heure actuelle au format 12h AM/PM
+        heure_actuel = now.strftime("%I:%M:%S %p")  
+        
+        # Affiche l'heure actuelle en réécrasant la ligne précédente (pour afficher une horloge en temps réel)
         print(heure_actuel, end="\r")  
+        
+        # Attend 1 seconde avant de réactualiser l'heure
         time.sleep(1)  
+        
+        # Si l'heure actuelle correspond à l'heure de l'alarme, affiche un message
         if heure_actuel == heure_reveil:
-            print("\nDebout, FEIGNAAAAASSEEEE !!!!! ")
+            print("\nDebout, FEIGNAAAAASSEEEE")
+
 def horloge():
-    heure_debut = input("Entrez l'heure de départ HH:MM:SS :")   
+    # Demande à l'utilisateur d'entrer une heure de départ au format HH:MM:SS
+    heure_debut = input("Entrez l'heure de départ HH:MM:SS :")    
+    
     try:
+        # Découpe l'heure entrée par l'utilisateur en heures, minutes et secondes
         h, m, s = map(int, heure_debut.split(':'))
+        
+        # Vérifie si l'heure, les minutes et les secondes sont valides
         if not (0 <= h < 24 and 0 <= m < 60 and 0 <= s < 60):
             print("Heure invalide. Veuillez entrer une heure valide .")
-            return
+            return  # Si l'heure est invalide, on quitte la fonction
     except ValueError:
+        # Si l'entrée n'est pas au bon format (par exemple "12:60:00"), on affiche une erreur
         print("Format invalide. Veuillez entrer l'heure au format HH:MM:SS .")
-        return 
+        return  # On quitte la fonction en cas d'erreur
+    
     while True:
-        print(f"{h:02}:{m:02}:{s:02}", end="\r")
+        # Crée une date avec l'heure actuelle, mais en remplaçant l'heure, les minutes et les secondes par celles de l'utilisateur
+        heure_formatee = datetime.now().replace(hour=h, minute=m, second=s).strftime("%I:%M:%S %p") 
+        
+        # Affiche l'heure formatée
+        print(heure_formatee, end='\r')
+        
+        # Attend 1 seconde avant de réactualiser l'affichage
         time.sleep(1)
+        
+        # Incrémente les secondes
         s += 1
+        # Si les secondes atteignent 60, on les remet à 0 et on incrémente les minutes
         if s == 60:
             s = 0
             m += 1
-        elif m == 60:
+        # Si les minutes atteignent 60, on les remet à 0 et on incrémente les heures
+        if m == 60:
             m = 0
             h += 1
-        elif h == 24:
+        # Si les heures atteignent 24, on les remet à 0
+        if h == 24:
             h = 0
+
 def menu():
-    while True:
-        print ("____MENU_DE_L'HORLOGE____")
-        print ("1 : Afficher l'heure actuelle ")
-        print ("2 : Régler une heure ")
-        print ("3 : Régler une alarme ")
-        print ("4 : Quitter ")
-        print ("_________________________")
+    while True:  # Ce "while True" permet de garder le menu affiché jusqu'à ce que l'utilisateur choisisse de quitter
+        print("______MENU_DE_L'HORLOGE______")
+        print("1 : Afficher l'heure actuelle ")
+        print("2 : Régler une heure ")
+        print("3 : Régler une alarme ")
+        print("4 : Quitter ")
+        print("_____________________________")
+        
+        # Demande à l'utilisateur de faire un choix dans le menu
         choix = input("Faites votre choix (1, 2, 3, 4) : ")
-        sleep(1)
-        os.system('cls')
-        if choix == "1" :
+        
+        sleep(1)  # Pause d'une seconde avant de continuer
+        os.system('cls')  # Nettoie l'écran pour réafficher le menu (fonction spécifique à Windows)
+
+        # Si l'utilisateur choisit "1", affiche l'heure actuelle
+        if choix == "1":
             print("\nIl est actuellement :")
-            afficher_heure(None)           
-        elif choix == "2" :
+            afficher_heure(None)  # Affiche l'heure actuelle sans définir d'alarme
+        # Si l'utilisateur choisit "2", lui permet de régler une horloge
+        elif choix == "2":
             print("\nVous êtes le maître du temps, choisissez l'heure que vous voulez :")
-            horloge() 
-        elif choix == "3" :
-            heure_reveil = alarme()
-            afficher_heure(heure_reveil)
-        elif choix == "4" :
+            horloge()  # Lance la fonction horloge pour régler une nouvelle heure
+        # Si l'utilisateur choisit "3", lui permet de régler une alarme
+        elif choix == "3":
+            heure_reveil = alarme()  # Lance la fonction alarme pour régler l'heure de l'alarme
+            afficher_heure(heure_reveil)  # Affiche l'heure actuelle et attend l'alarme
+        # Si l'utilisateur choisit "4", quitte le programme
+        elif choix == "4":
             print("Au revoir !")
-            break
+            exit()  # Quitte le programme
         else:
+            # Si l'utilisateur fait un choix invalide, on lui demande de recommencer
             print("Choix invalide. Essayez à nouveau.")
+
+# Appelle la fonction menu pour démarrer le programme
 menu()
