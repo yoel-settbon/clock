5e631f4e397a153e9ddaaacf83471f091a14e3b0
yoel-settbon <yoel-settbon@laplateforme.io>
2025-01-12 15:14:50 +0100

    feat: ajout de l'alarme et des keyinterrupt au programme + commentaires correspondants

--- clockdev.py
+++ clockdev.py
@@ -1,93 +1,124 @@
-from datetime import datetime
-import time
-import os
-from time import sleep
-import pygame  # Importer pygame pour jouer des fichiers audio
+from datetime import datetime   # Importation pour la manipulation du temps
+import time                     # Importation pour gérer les pauses
+import os                       # Importation des commandes système
+from time import sleep          # Importation des pauses pour faire des pause dans le programme
+import pygame                   # Importation pour la lecture du fichier .wav
 pygame.init()
 
-alarme_sound = pygame.mixer.Sound("alarm.wav")
+alarme_sound = pygame.mixer.Sound("alarm.wav") # Définition d'une variable et récupération du fichier .wav
+
 
 def alarme():
-    heure_reveil = input("Régler un réveil (HH:MM:SS) :")
+    # demande à l'utilisateur de régler une alarme
+    heure_reveil = input("Régler un réveil (HH:MM:SS AM/PM) :")
     try:
-        time.strptime(heure_reveil, "%H:%M:%S")
+    # s'assure que l'heure entrée est au format voulu AM/PM        time.strptime(heure_reveil, "%I:%M:%S %p")
         return heure_reveil
     except ValueError:
+    # si le format est invalide un message d'erreur s'affiche et redemande de rentrer l'heure
         print("Heure non valide, entrer une heure valide.")
         return alarme()
 
+
 def afficher_heure(heure_reveil):
+    # Utilisation try/except pour appliquer un keyinterrupt
     try:
+            # Boucle infinie pour afficher l'heure actuelle
         while True:
+            # L'heure du système est récupérée et utilisée
             now = datetime.now()
-            heure_actuel = now.strftime("%A %d %m %Y --- %I:%M:%S %p ---")            
-            print(heure_actuel, end="\r")
+            # Conversion de l'heure en format AM/PM
+            heure_actuelle = now.strftime("%I:%M:%S %p")  # Format AM/PM
+            print(heure_actuelle, end="\r")  # Affiche l'heure en temps réel
             time.sleep(1)
 
-            if heure_actuel == heure_reveil:
-                print("\nDebout, FEIGNAAAAASSEEEE !!!!! ")
+            # Conditions d'activation de l'alarme
+            if heure_reveil and heure_actuelle == heure_reveil:
+                # Affichage du message de l'alarme 
+                print("\nDebout, FEIGNAAAAASSEEEE")
+                # Activation du fichier .wav de l'alarme
                 alarme_sound.play()
+                # Gestion du temps de l'affichage du message, correspondant à la durée du fichier .wav
                 sleep(16)
-
-            os.system('cls')
+                break
     except KeyboardInterrupt:
+        # Stoppe le son et retourne au menu
         pygame.mixer.stop()
         print("\nAffichage de l'heure interrompu par l'utilisateur. Retour au menu.")
+        return
 
 
 def horloge():
+    # l'utilisateur peux rentrer l'heure de départ souhatée
+    heure_debut = input("Entrez l'heure de départ HH:MM:SS :")
     try:
-        heure_debut = input("Entrez l'heure de départ HH:MM:SS :")   
-        try:
-            h, m, s = map(int, heure_debut.split(':'))
-            if not (0 <= h < 24 and 0 <= m < 60 and 0 <= s < 60):
-                print("Heure invalide. Veuillez entrer une heure valide .")
-                return
-        except ValueError:
-            print("Format invalide. Veuillez entrer l'heure au format HH:MM:SS .")
-            return 
-        
+            # Permet de séparer l'heure saisie par l'utilisateur au format HH:MM:SS
+        h, m, s = map(int, heure_debut.split(':'))
+        if not (0 <= h < 24 and 0 <= m < 60 and 0 <= s < 60):
+            # Si l'heure est invalide, on redemande à l'utilisateur de rentrer une heure valide
+            print("Heure invalide. Veuillez entrer une heure valide :")
+            return horloge()
+    except ValueError:
+            # Si l'entrée n'est pas au bon format, on affiche un message d'erreur, et on redemande à l'utilisateur d'entrer une heure valide
+        print("Format invalide. Veuillez entrer l'heure au format HH:MM:SS :")
+        return horloge()
+                # Utilisation d'un try/except autour de la boucle pour mettre en place le keyinterrupt et retour au menu
+    try:
         while True:
-            print(f"{h:02}:{m:02}:{s:02}", end="\r")
+                # Créé une date avec l'heure actuelle, remplaçant les heures, minutes, et secondes par celles entrées par l'utilisateur
+            heure_formatee = datetime.now().replace(hour=h, minute=m, second=s).strftime("%I:%M:%S %p")
+                # Affiche l'heure formatée
+            print(heure_formatee, end='\r')
+                #Actualise l'heure toutes les secondes
             time.sleep(1)
+                # Incrémente les secondes
             s += 1
+                # Dès que les secondes et minutes atteignent 60, elles sont remises à 0 et on incrémente les minutes et les heures
             if s == 60:
                 s = 0
                 m += 1
-            elif m == 60:
+            if m == 60:
                 m = 0
                 h += 1
-            elif h == 24:
+                # Quand les heures atteignent 24, elles sont remises à 0
+            if h == 24:
                 h = 0
-            os.system('cls') 
-    
+            os.system('cls')  # Efface l'écran pour un affichage propre
+
     except KeyboardInterrupt:
         print("\nHorloge interrompue par l'utilisateur. Retour au menu.")
-        return # Efface l'écran pour un affichage propre
+        return
+
 
 def menu():
     while True:
-        print ("____MENU_DE_L'HORLOGE____")
-        print ("1 : Afficher l'heure actuelle ")
-        print ("2 : Régler une heure ")
-        print ("3 : Régler une alarme ")
-        print ("4 : Quitter ")
-        print ("_________________________")
+            # Affichage du menu de l'horloge
+        print("______MENU_DE_L'HORLOGE______")
+        print("1 : Afficher l'heure actuelle ")
+        print("2 : Régler une horloge ")
+        print("3 : Régler une alarme ")
+        print("4 : Quitter ")
+        print("_____________________________")
+            # Input permettant à l'utilisateur de faire un choix dans le menu
         choix = input("Faites votre choix (1, 2, 3, 4) : ")
-        sleep(1)
         os.system('cls')
-        if choix == "1" :
+            # Une fois le choix fait, l'utilisateur est redirigé vers la fonction correspondante
+        if choix == "1":
             print("\nIl est actuellement :")
-            afficher_heure(None)           
-        elif choix == "2" :
+            afficher_heure(None)
+        elif choix == "2":
             print("\nVous êtes le maître du temps, choisissez l'heure que vous voulez :")
-            horloge() 
-        elif choix == "3" :
+            horloge()
+        elif choix == "3":
             heure_reveil = alarme()
             afficher_heure(heure_reveil)
-        elif choix == "4" :
+        elif choix == "4":
+            # Si l'utilisateur choisit de quitter, un message d'au revoir s'affiche et le programme se ferme
             print("Au revoir !")
-            break
+            exit()
         else:
             print("Choix invalide. Essayez à nouveau.")
+
+
+# Appelle la fonction menu de l'horloge
 menu()
